bb38bd9d4bf380101cad96a376f6e2d7d3514b5e
webdevmahdi <mahdihasan01705259@gmail.com>
2025-05-12 00:37:41 +0600

    OOP keywords

--- module3/3.5.ts
+++ module3/3.5.ts
@@ -1,3 +1,26 @@
 {
-    
+  //
+  class BankAccount {
+    id: number;
+    name: string;
+    private _balance: number;
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+
+    deposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+    getBalance() {
+      return this._balance;
+    }
+  }
+
+  const user = new BankAccount(11, "Mahdi", 100000);
+  user.deposit(400);
+
+  //
 }
